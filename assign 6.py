84b4eff71ec0c4aadfc0ab39e5d7e74374ef3094
rhythmshah16 <66407601+rhythmshah16@users.noreply.github.com>
2020-06-17 11:50:42 +0530

    Update assign 6.py

--- assign 6.py
+++ assign 6.py
@@ -4,9 +4,7 @@ import numpy as np
 while True:
     img = cv2.imread('B99.jpg')
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
     kernel = np.ones((5,5))
-
     erode = cv2.erode(img_gray,kernel)
     dilate = cv2.dilate(img_gray,kernel)
     closing = cv2.morphologyEx(img_gray,cv2.MORPH_CLOSE,kernel)
